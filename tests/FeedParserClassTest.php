ce19bd19e1eb63d45824a84e6cbf9ad3f42aed9b
marcin-kowalczyk <marcin@incitosystems.com>
2018-11-21 16:47:58 +0100

    php5.6 support updates

--- tests/FeedParserClassTest.php
+++ tests/FeedParserClassTest.php
@@ -4,14 +4,14 @@ use PHPUnit\Framework\TestCase;
 
 class FeedParserClassTest extends TestCase
 {
-    public function testIsThereAnySyntaxError(): void
+    public function testIsThereAnySyntaxError()
     {
         $var = new Thsgroup\FeedParser\FeedParserClass();
         $this->assertInternalType('object', $var);
         unset($var);
     }
 
-    public function testInitialMethod(): void
+    public function testInitialMethod()
     {
         $var = new Thsgroup\FeedParser\FeedParserClass();
         $this->assertEquals($var->initialMethod(), 'Test');
